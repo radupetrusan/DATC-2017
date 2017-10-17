f094b710576b2b8e2a388da19ee39992dc14f8e2
dturcian <turciandarius@yahoo.com>
2017-10-17 21:02:06 +0300

    Curs tema1

---
+++ Turcian Darius/CURS/TEMA1/Tema1/Tema1/Endpoints/Content.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Tema1.Endpoints
+{
+    public class Content
+    {
+        [JsonProperty(PropertyName = "self")]
+        public Reference Self { get; set; }
+
+        [JsonProperty(PropertyName = "style")]
+        public Reference Style { get; set; }
+
+        [JsonProperty(PropertyName = "brewery")]
+        public List<Brewery> Brewery { get; set; }
+    }
+}
---
+++ Turcian Darius/CURS/TEMA1/Tema1/Tema1/Endpoints/Reference.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Tema1.Endpoints
+{
+    public class Reference
+    {
+        [JsonProperty(PropertyName = "href")]
+        public string Href { get; set; }
+
+        [JsonProperty(PropertyName = "templated")]
+        public bool Templated { get; set; }
+    }
+}
