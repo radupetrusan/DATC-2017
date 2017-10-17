5dc327cc0732ff7c5c57f69aad021733fb6f247f
biancatrutiu <32415808+biancatrutiu@users.noreply.github.com>
2017-10-17 20:26:46 +0300

    Add files via upload

---
+++ Bianca_Trutiu/Curs/Tema 1/TrutiuBiancaTema1/TrutiuBiancaTema1/Beers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrutiuBiancaTema1
+{
+    public class Beers
+    {
+        private string href;
+        
+        public string Href { get => href; set => href = value; }
+       
+        
+    }
+}
