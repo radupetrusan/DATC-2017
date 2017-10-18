ba5a265f6115432bfc0f0b5051181e36659d8ea2
remusjurj <32365850+remusjurj@users.noreply.github.com>
2017-10-18 22:04:51 +0300

    Tema 1

---
+++ REMUS JURJ/CURS/Tema1/TemaDATC/TemaDATC/MyJObject.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemaDATC
+{
+    class MyJObject
+    {
+        // GET DATA FROM URL
+        public static string GET(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.ContentType = "application/hal+json";
+            try
+            {
+                WebResponse response = request.GetResponse();
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (WebException ex)
+            {
+                WebResponse errorResponse = ex.Response;
+                using (Stream responseStream = errorResponse.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
+                    String errorText = reader.ReadToEnd();
+                }
+            }
+            return "Error";
+        }
+
+        public static void POST(string url, int Id, string Name)
+        {
+            string newUrl = url.Substring(0,34) +"/beers";
+            Beers b = new Beers(Id, Name);
+
+            
+            StringContent content = new StringContent(JsonConvert.SerializeObject(b));
+
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            content.Headers.ContentType = new MediaTypeWithQualityHeaderValue("application/json");
+            var clientResponse = client.PostAsync(newUrl, content).Result;
+            Console.WriteLine(clientResponse);
+        }
+    }
+}
