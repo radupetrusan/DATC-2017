898d5640a5594779fd9fc2d53f179ca47cff87a5
PatrikStepan <stepanpatrik@gmail.com>
2017-10-18 23:04:53 +0300

    refactor states

--- Stepan_Patrik/CURS/TEMA1/Hal.Client/Hal.Client/Program.cs
+++ Stepan_Patrik/CURS/TEMA1/Hal.Client/Hal.Client/Program.cs
@@ -9,92 +9,50 @@ using System.Threading.Tasks;
 namespace Hal.Client
 {
 
-    public enum UIState {
-        UI_BREWERIES,
-        UI_BEERS,
-        UI_DETAILS,
-        UI_NONE,
-    };
-
     class Program
     {
         static void Main(string[] args)
         {
-            var client = new HttpClient();
-            client.BaseAddress =  new Uri("http://datc-rest.azurewebsites.net/");
-            client.DefaultRequestHeaders.Add("Accept", "application/hal+json"); 
+            UIStateType gState = UIStateType.MAIN_MENU;
+            UIStateType gNextState = UIStateType.ERROR;
 
-            UIState gState = UIState.UI_NONE;
-            List<Brewery> gBreweries = null;
-            List<Beer> gBeers = null;
-
-            string command = null;
-            int currentBrewery = 0;
-            int currentBeer = 0;
-            
             while(true)
             {
-                switch(gState)
-                {
-                    case UIState.UI_NONE:
-                        {
-                            gBreweries = Brewery.GetBrewriesRequest(client);
-                            Brewery.PrintList(gBreweries);
-
-                            // TODO: fix command reading !
-                            try
-                            {
-                                command = Console.ReadLine();
-                                currentBrewery = Int32.Parse(command);
-
-                                gState = UIState.UI_BREWERIES;
-                            }
-                            catch { }
-                            break;
-                        }
-                    case UIState.UI_BREWERIES:
-                        {
-                            // TODO: check out of bounds somewhere
-                            var currentRes = gBreweries[currentBrewery].links.beers.href;
-
-                            gBeers = Beer.GetBeersRequest(client, currentRes);
-
-                            if(gBeers != null)
-                                Beer.PrintList(gBeers);
-                            else
-                            {
-                                gState = UIState.UI_NONE;
-                                Console.WriteLine("Invalid resource!\n");
-                                break;
-                            }
-
-                            command = Console.ReadLine();
-                            try
-                            {
-                                currentBeer = Int32.Parse(command);
-                                gState = UIState.UI_DETAILS;
-                            }
-                            catch
-                            {
-                                // Currently this serves as 'back' command also
-                                gState = UIState.UI_NONE;
-                            }
-                            break;
-                        }
-                    case UIState.UI_DETAILS:
-                        Beer.PrintDetails(gBeers[currentBeer]);
-
-                        gState = UIState.UI_BREWERIES;
-                        Console.ReadLine();
-                        break;
-                    default:
-                        Console.WriteLine("State Error!\n");
-
-                        break;
-                }
+                Run(gState, out gNextState);
+                gState = gNextState;
             }
+        }
 
-            Console.ReadLine();
+        static private void Run(UIStateType state, out UIStateType nextState)
+        {
+            nextState = UIStateType.ERROR;
+            switch(state)
+            {
+                case UIStateType.MAIN_MENU:
+                    nextState = UIMainMenuState.Execute();
+                    break;
+                case UIStateType.LIST_ALL_BREWERIES:
+                    nextState = UIBreweriesListState.Execute();
+                    break;
+                case UIStateType.LIST_ALL_BEERS:
+                    nextState = UIBeerList.Execute();
+                    break;
+                case UIStateType.LIST_BEERS_PER_BREWERY:
+                    nextState = UIBeersPerBrewery.Execute();
+                    break;
+                case UIStateType.SEARCH_BEER_BY_NAME:
+                    break;
+                case UIStateType.BEER_DETAILS:
+                    nextState = UIBeerDetails.Execute();
+                    break;
+                case UIStateType.ADD_BEER:
+                    nextState = UIAddBeer.Execute();
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("ERROR");
+                    break;
+            }
         }
     }
 }
