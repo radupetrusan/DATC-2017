4be5fe29f1970cc35455b1c5fd786e593e35ccfb
dtorje <32431653+dtorje@users.noreply.github.com>
2017-10-19 02:31:52 +0300

    Tema1-CURS
    Client aplicatie consola pentru un API REST (GET&POST)

---
+++ TORJE David/CURS/TEMA 1/Hal.Client/Hal.Client/Breweries.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hal.Client
+{
+    class Breweries
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string _links { get; set; }
+
+    }
+}
