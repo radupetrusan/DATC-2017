0ccbada3e4b72c292929aac074df1f897d5b7a1c
Nicula Andreea <32903338+nicula@users.noreply.github.com>
2017-10-19 23:15:17 +0300

    Update and rename App.config to Beer.cs

---
+++ Nicula Andreea/Curs/Tema1/Tema1/Beer.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema1
+{
+    class Beer
+    {
+         public string Id { get; set; }
+         public string Name { get; set; }
+         public string Category { get; set; }
+    }
+}
+
