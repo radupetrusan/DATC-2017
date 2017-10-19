ec4017b91bb2226a0835a6fbed201eee23ce300e
alexandragrama <alexandra.e.grama@gmail.com>
2017-10-19 20:23:57 +0300

    Tema 1

---
+++ Grama Elena-Alexandra/CURS/TEMA1/Tema1_Grama/Tema1_Grama/RootObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema1_Grama
+{
+    public class RootObject
+    {
+        public Links _links { get; set; }
+        public Embedded _embedded { get; set; }
+                      
+        
+        
+    }
+}
