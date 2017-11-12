0526ea89c0d176ea79d04dddfd61eeb199544168
prodaniucandrei <prodaniucandrei@gmail.com>
2017-11-12 12:07:12 +0200

    Tema 2 - initial - view/add comment functionality added

---
+++ Prodaniuc Andrei/CURS/Tema 2/02_AlbumFoto-cu-worker/AlbumPhoto/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using AlbumPhoto.Models;
+using AlbumPhoto.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlbumPhoto.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var service = new AlbumFotoService();
+            Session["poze"] = service.GetPoze();
+            return View(Session["poze"]);
+        }
+
+        [HttpPost]
+        public ActionResult IncarcaPoza(HttpPostedFileBase file)
+        {
+            var service = new AlbumFotoService();
+            if (file!=null && file.ContentLength > 0)
+            {
+                service.IncarcaPoza("guest", file.FileName, file.InputStream);
+            }
+            Session["poze"] = service.GetPoze();
+            return View("Index", Session["poze"]);
+        }
+
+        [HttpPost]
+        public ActionResult PosteazaComentariu(string text, string poza)
+        {
+            var service = new AlbumFotoService();
+            if (!string.IsNullOrEmpty(text) && !string.IsNullOrEmpty(poza))
+            {
+                service.PosteazaComentariu(Guid.NewGuid().ToString(), poza, text);
+            }
+            Session["poze"] = service.GetPoze();
+            return View("Index", Session["poze"]);
+        }
+
+        public ActionResult ShareAccess()
+        {
+            return View();
+        }
+    }
+}
