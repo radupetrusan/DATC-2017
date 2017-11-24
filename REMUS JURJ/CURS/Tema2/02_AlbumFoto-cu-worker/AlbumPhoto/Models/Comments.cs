99e51eb27768abd55e4f156c713276a34f0ce0d5
remusjurj <32365850+remusjurj@users.noreply.github.com>
2017-11-24 23:31:57 +0200

    Tema2
    Tema2

---
+++ REMUS JURJ/CURS/Tema2/02_AlbumFoto-cu-worker/AlbumPhoto/Models/Comments.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlbumPhoto.Models
+{
+    public class Comments
+    {
+        public string Comment { get; set; }
+        public string User { get; set; }
+        public string Picture { get; set; }
+    }
+}
