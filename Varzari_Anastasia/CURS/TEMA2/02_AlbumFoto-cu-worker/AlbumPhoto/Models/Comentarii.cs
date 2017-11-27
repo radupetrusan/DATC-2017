5098c7025f228449a0edde4c0c08b17958c1559a
VarzariAnastasia <varzarianastasia@yahoo.ro>
2017-11-27 22:32:09 +0200

    Adaugare Comentarii
    Adaugare Comentarii

---
+++ Varzari_Anastasia/CURS/TEMA2/02_AlbumFoto-cu-worker/AlbumPhoto/Models/Comentarii.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlbumPhoto.Models
+{
+    public class Comentarii
+    {
+        public string Text { get; set; }
+        public string MadeBy { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
