28b0b7d169ff6d6e4b9e16220ce620d62cff9a0d
Voloacă Maria-Alexandra <32413796+mariavoloaca@users.noreply.github.com>
2017-11-27 18:03:07 +0200

    tema2

---
+++ Voloaca Maria/Curs/Tema2/02_AlbumFoto-cu-worker Voloaca Maria/AlbumPhoto/Models/Comm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlbumPhoto.Models
+{
+    public class Comm
+    {
+        public string Text { get; set; }
+        public string MadeBy { get; set; }
+        public string Description { get; set; }
+    }
+}
